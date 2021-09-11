2a5f5b94788078c63761ab4f804ea06250e9dd59
caiohscruz <caiohscruz@hotmail.com>
2021-09-10 22:51:46 -0300

    feat delete

--- index.js
+++ index.js
@@ -67,7 +67,11 @@ app.get("/game/:id", (req, res) => {
 // create a game - begin
 app.post("/game", (req, res) => {
 
-    var {title, year, price} = req.body
+    var {
+        title,
+        year,
+        price
+    } = req.body
 
     DB.games.push({
         id: 234,
@@ -80,6 +84,23 @@ app.post("/game", (req, res) => {
 })
 // create a game - end
 
+// delete a game - begin
+app.delete("/game/:id", (req, res) => {
+    if (isNaN(req.params.id)) {
+        res.sendStatus(400)
+    } else {
+        var id = parseInt(req.params.id)
+        var index = DB.games.findIndex(g => g.id == id)
+
+        if (index == -1) {
+            res.sendStatus(404)
+        } else {
+            DB.games.splice(index, 1)
+            res.sendStatus(200)
+        }
+    }
+})
+// delete a game - begin
 
 app.listen(45789, () => {
     console.log("API RODANDO")
