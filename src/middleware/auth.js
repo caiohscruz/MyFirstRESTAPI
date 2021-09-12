4c4dfebfa8ef173a37ddf42ce64e09d5a093922b
caiohscruz <caiohscruz@hotmail.com>
2021-09-12 16:52:32 -0300

    add token validation

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,9 +1,33 @@
-function auth(req, res, next){
-    
+function auth(req, res, next) {
+
+    const jwt = require("jsonwebtoken")
+    const jwtSecret = process.env.JWTSECRET
+
     const authToken = req.headers["authorization"]
-    console.log(authToken)
-    next()
-    
+
+    if (authToken != undefined) {
+        authToken = authToken.split(" ")[1]
+
+        jwt.verify(authToken, jwtSecret, (err, data)=>{
+            if(err){
+                res.status(401)
+                res.json({
+                    err: "Token inválido"
+                })
+            }else{
+                req.token = authToken
+                req.loggedUser= {id: data.id, username: data.username}
+                next()
+            }
+        })
+
+    } else {
+        res.status(401)
+        res.json({
+            err: "Token inválido"
+        })
+    }
+
 }
 
 module.exports = auth
